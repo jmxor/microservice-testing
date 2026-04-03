91d80839eaaac6d30b127a0ff0152958d5eb80af
jmxor <jamesmrippeth@gmail.com>
2026-04-03 10:28:37 +0100

    Add validation to the SecurityProperties record

--- spring-example/src/main/java/org/jmxor/springExample/config/SecurityProperties.java
+++ spring-example/src/main/java/org/jmxor/springExample/config/SecurityProperties.java
@@ -1,12 +1,20 @@
 package org.jmxor.springExample.config;
 
+import jakarta.validation.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
+@Validated
 @ConfigurationProperties(prefix = "app.security")
 public record SecurityProperties (
+    @NotEmpty(message = "Configuration property CORS allowed origins cannot be empty")
     List<String> corsAllowedOrigins,
+
+    @NotEmpty(message = "Configuration property Public paths cannot be empty")
     List<String> publicPaths,
+
+    @NotEmpty(message = "Configuration property JWT allowed audiences cannot be empty")
     List<String> jwtAllowedAudiences
 ) {}
